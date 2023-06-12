7337b6e67c13459962691069874a1ec46b9e945e
jordanfwoods <jordan.f.woods@gmail.com>
2023-06-12 09:43:55 -0700

    Updating tmux configuration to have watson. Fixed git coloring

--- tmux/watson.sh
+++ tmux/watson.sh
@@ -2,10 +2,10 @@
 output=""
 [[ -z $(command -v watson) ]] && echo "" && exit
 stat=$(watson status)
-[[ $stat = No* ]] && echo "#[fg=colour15]No Watson Project Started | " && exit
-output="#[fg=colour2]$(echo $stat | cut -d " " -f 2)"
-[[ $stat == *"["* ]] && output="$output #[fg=colour15][#[fg=colour28]"
+[[ $stat = No* ]] && echo "#[fg=colour147]No Watson Project Started | " && exit
+output="#[fg=colour172]$(echo $stat | cut -d " " -f 2)"
+[[ $stat == *"["* ]] && output="$output #[fg=colour15][#[fg=colour147]"
 [[ $stat == *"["* ]] && output="$output$(echo $stat | cut -d ] -f 1 | cut -d [ -f 2)#[fg=colour15]]"
-echo "$output | "
+echo "$output#[fg=colour15] | "
 # "#[fg=colour2]#(watson status | cut -d \" \" -f 2) "\
 # "#[fg=colour15][#[fg=colour28]#(watson status | cut -d ] -f 1 | cut -d [ -f 2)#[fg=colour15]] | "\
